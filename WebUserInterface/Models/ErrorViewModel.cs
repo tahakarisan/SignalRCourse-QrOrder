e15024c4d4ae8acafdf9d772e5a0faf2a7bb1e23
tahakarisan <th.omer3737@gmail.com>
2025-07-01 12:35:31 +0300

    Web UI düzeltildi mvc için component oluşturuldu

---
+++ WebUserInterface/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebUserInterface.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
