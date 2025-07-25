09af368b9fdfa460d042082c0cdce8ae40df05f7
tahakarisan <th.omer3737@gmail.com>
2025-07-25 22:26:48 +0300

    Layout Tamamlandı

---
+++ WebUserInterface/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUserInterface.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebUserInterface/ViewComponents/UILayoutComponents/_UILayoutScriptPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUserInterface.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutScriptPartialComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
