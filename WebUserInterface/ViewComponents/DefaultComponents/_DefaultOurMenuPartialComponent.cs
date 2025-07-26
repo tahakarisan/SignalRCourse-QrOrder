b5af8ad813e221f91fc328f086f25cad1b47361d
tahakarisan <th.omer3737@gmail.com>
2025-07-26 23:52:17 +0300

    UI kısmında parçalanarak mimarimize oturtuldu

---
+++ WebUserInterface/ViewComponents/DefaultComponents/_DefaultOurMenuPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUserInterface.ViewComponents.DefaultComponents
+{
+    public class _DefaultOurMenuPartialComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebUserInterface/ViewComponents/DefaultComponents/_DefaultTestimonialPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUserInterface.ViewComponents.DefaultComponents
+{
+    public class _DefaultTestimonialPartialComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
