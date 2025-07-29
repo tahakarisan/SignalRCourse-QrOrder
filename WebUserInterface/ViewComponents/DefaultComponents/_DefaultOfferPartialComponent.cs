951763c51097fe9136da35517e08d517ea1604c6
tahakarisan <th.omer3737@gmail.com>
2025-07-29 11:46:24 +0300

    Gerçek Veriler Getirildi UI Düzenlemeler Getirildi

--- WebUserInterface/ViewComponents/DefaultComponents/_DefaultOfferPartialComponent.cs
+++ WebUserInterface/ViewComponents/DefaultComponents/_DefaultOfferPartialComponent.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using WebUserInterface.Dtos;
 
 namespace WebUserInterface.ViewComponents.DefaultComponents
 {
     public class _DefaultOfferPartialComponent:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultOfferPartialComponent(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44356/api/Discount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+                return View(values);
+            }
+            return View(new List<ResultDiscountDto>());
         }
     }
 }
--- WebUserInterface/ViewComponents/DefaultComponents/_DefaultOurMenuPartialComponent.cs
+++ WebUserInterface/ViewComponents/DefaultComponents/_DefaultOurMenuPartialComponent.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUserInterface.Dtos;
 
 namespace WebUserInterface.ViewComponents.DefaultComponents
 {
     public class _DefaultOurMenuPartialComponent:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultOurMenuPartialComponent(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+        public async  Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44356/api/ProductContoller/getProductWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View(new List<ResultProductDto>());
         }
     }
 }
